21dfb7fa14905d760d7f05186f3933b29f6a13d2
Anand Chauhan <anandchauhan9628@gmail.com>
2025-02-14 10:53:37 +0530

    soon adding more

--- JS/OwnArrayPolyfills/myReduce.js
+++ JS/OwnArrayPolyfills/myReduce.js
@@ -1,14 +1,5 @@
 const array1 = [1, 2, 3, 4];
 
-// // 0 + 1 + 2 + 3 + 4
-// const initialValue = 0;
-// const sumWithInitial = array1.reduce(
-//   (accumulator, currentValue) => accumulator + currentValue,
-//   initialValue,
-// );
-
-// console.log(sumWithInitial);
-// Expected output: 10
 
 if(!Array.prototype.myReduce){
     Array.prototype.myReduce = function(callback){
